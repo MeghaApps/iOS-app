d26814928749a8e93464c8e0f595722ccc8a60d5
Karthikeyan <karthikeyan.k2000@gmail.com>
2021-05-17 12:23:53 +0530

    Implemented UI

--- megha/ViewController.swift
+++ megha/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var buttonOutput: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func didTapView(_ sender: UITapGestureRecognizer) {
+        print("View Tapped", sender)
+    }
+    
+    @IBAction func didTapButton(_ sender: UIButton) {
+        print("Button Tapped", sender)
+    }
+    
 }
-
