62678e660ab617c7964996707c52e93888d157e5
Karthikeyan <karthikeyan.k2000@gmail.com>
2021-09-15 22:46:59 +0530

    Completed Text Display

--- megha/ViewController.swift
+++ megha/ViewController.swift
@@ -13,19 +13,19 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
     @IBOutlet weak var labelOutput: UILabel!
     @IBOutlet weak var imageOutput: UIImageView!
     @IBOutlet weak var buttonRestart: UIButton!
-    private var classifier: ImageClassifier?
+    private var result: Result?
+    
+    private var modelDataHandler: ModelDataHandler? =
+        ModelDataHandler(modelFileInfo: MobileNet.modelInfo, labelsFileInfo: MobileNet.labelsInfo)
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        ImageClassifier.newInstance { result in
-            switch result {
-            case let .success(classifier):
-                self.classifier = classifier
-            case .error(_):
-                self.labelOutput.text = "Failed to initialize."
+        guard modelDataHandler != nil else {
+              fatalError("Model set up failed")
             }
-        }
     }
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
@@ -39,15 +39,14 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
         imageOutput.image = image
         labelOutput.text = "Please wait... processing!"
         
-        guard let classifier = self.classifier else { return }
-        classifier.classify(image: image) { result in
-            switch result {
-            case let .success(classificationResult):
-                self.labelOutput.text = classificationResult
-            case .error(_):
-                self.labelOutput.text = "Failed to classify drawing."
-            }
+        guard let buffer = CVImageBuffer.buffer(from: image) else {
+              return
         }
+        
+        result = modelDataHandler?.runModel(onFrame: buffer)
+        
+        //labelOutput.text = String(describing: result?.inferences)
+        labelOutput.text = "The cloud is of the type \(getFullLabelText(label: (result?.inferences[0].label)!)) with a probability of \((result?.inferences[0].confidence)! * 100).\n\(getLabelPrediction(label: (result?.inferences[0].label)!))"
     }
     
     @IBAction func didTapView(_ sender: UITapGestureRecognizer) {
@@ -69,4 +68,61 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
         present(imagePickerController, animated: true)
     }
     
+    func getFullLabelText(label: String) -> String {
+        switch label {
+        case "Ci":
+            return "Cirrus"
+        case "Cs":
+            return "Cirrostratus"
+        case "Cc":
+            return "Cirrocumulus"
+        case "Ac":
+            return "Altocumulus"
+        case "As":
+            return "Altostratus"
+        case "Cu":
+            return "Cumulus"
+        case "Cb":
+            return "Cumulonimbus"
+        case "Ns":
+            return "Nimbostratus"
+        case "Sc":
+            return "Stratocumulus"
+        case "St":
+            return "Stratus"
+        case "Ct":
+            return "Contrail"
+        default:
+            return "error"
+        }
+    }
+    
+    func getLabelPrediction(label: String) -> String {
+        switch label {
+        case "Ci":
+            return "A warm front is approaching."
+        case "Cs":
+            return "A storm is coming."
+        case "Cc":
+            return "The weather is about to change!"
+        case "Ac":
+            return "Rain is coming soon!"
+        case "As":
+            return "Rain, incoming."
+        case "Cu":
+            return "Fair weather!"
+        case "Cb":
+            return "Thunderstorms may be due."
+        case "Ns":
+            return "Rain / Fog incoming."
+        case "Sc":
+            return "Bad weather incoming."
+        case "St":
+            return "Light rain."
+        case "Ct":
+            return "Airplanes!"
+        default:
+            return "error"
+        }
+    }
 }
